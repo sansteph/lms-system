79d0c48439d39f11f323d3a03859bc66b1afcf91
SanSteph <sanjustephen69@gmail.com>
2026-07-23 15:28:46 +0530

    UI tweaks 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,7 +40,7 @@
                             <path d="M20.45 20.45h-3.56v-5.57c0-1.33-.02-3.04-1.85-3.04-1.86 0-2.14 1.45-2.14 2.95v5.66H9.34V9h3.42v1.56h.05c.48-.9 1.64-1.85 3.37-1.85 3.6 0 4.27 2.37 4.27 5.46v6.28zM5.32 7.43a2.06 2.06 0 1 1 0-4.12 2.06 2.06 0 0 1 0 4.12zM7.1 20.45H3.53V9H7.1v11.45z"/>
                         </svg>
                     </a>
-                    <a href="https://www.instagram.com/tinkedge_/?hl=en"
+                    <a href="https://www.instagram.com/tinkedge_/"
                        target="_blank"
                        rel="noopener"
                        title="Instagram">
